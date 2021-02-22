f6f68439e5c124c3fe15a79f5410adc8376e77b6
Angelee Shaw <angeleefshaw@gmail.com>
2021-02-21 19:53:31 -0500

    Adding multimedia proj

---
+++ script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     // $(window).resize(function() {
+//     //     myFunction();
+//     // });
+//     // myFunction();
+
+//     $(".burger").click(function() {
+//         $("nav.mobile").addClass("show");
+//     });
+
+//     $(".close span").click(function() {
+//         $("nav.mobile").removeClass("show");
+//     });
+// });
